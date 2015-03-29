1a0c9b5a155c88d98d17cca054cf893faaa4cdc5
Paul Crofts <pmcrofts@margic.com>
2015-03-29 08:27:47 -0700

    adding registry tests by type and with guice named annotation

--- src/test/java/com/margic/camel/GuiceRegistryTest.java
+++ src/test/java/com/margic/camel/GuiceRegistryTest.java
@@ -3,6 +3,8 @@ package com.margic.camel;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
 import com.margic.pihex.camel.BindCamelRegistry;
 import com.margic.pihex.camel.context.GuiceRegistry;
 import org.junit.Test;
@@ -26,10 +28,29 @@ public class GuiceRegistryTest {
         GuiceRegistry registry = new GuiceRegistry(injector);
         Object object = registry.lookupByName("myBean");
         assertNotNull(object);
+        TestClass testClass = registry.lookup("myBean", TestClass.class);
+        assertNotNull(testClass);
     }
 
     @BindCamelRegistry(ref = "myBean")
     static class TestClass{
 
+    }
+
+    @Test
+    public void testBindCamelRegistryNamedAnnotation(){
+        Injector injector = Guice.createInjector(new AbstractModule() {
+            @Override
+            protected void configure() {
+                bind(String.class).annotatedWith(Names.named("testName")).toInstance("My String Value");
+            }
+        });
+
+        GuiceRegistry registry = new GuiceRegistry(injector);
+        String testString = (String)registry.lookup("testName");
+        assertEquals("My String Value", testString);
+
+        testString = registry.lookup("testName", String.class);
+        assertEquals("My String Value", testString);
     }
 }
